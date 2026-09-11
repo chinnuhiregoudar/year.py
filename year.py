ee4c4eb42919381a227b9bbc96586991785c3726
chinnuhiregoudar <chinnuhiregoudar>
2026-09-11 10:29:42 +0530

    Saving the file to github

---
+++ year.py
@@ -0,0 +1,9 @@
+year = int(input("Enter a year: "))
+if (year % 400 == 0):
+     print(It's a leap year)
+elif(year % 100 == 0):
+    print(It's a leap year)
+elif(year % 4 == 0):
+    print(It's a leap year) 
+    else:
+    print(It's not a leap year)
